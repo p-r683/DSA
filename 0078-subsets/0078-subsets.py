5d8119a2b6e82b28fb4772b062066f629fc54c8e
Pranshu Verma <vpranshu048@gmail.com>
2026-08-24 19:03:29 +0530

    Time: 3 ms (7.28%), Space: 19.5 MB (28.15%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,14 @@
 class Solution:
+
+    def solve(self,ind,subset,nums,result):
+            if ind>=len(nums):
+                result.append(subset.copy())
+                return
+            subset.append(nums[ind])
+            self.solve(ind+1,subset,nums,result)
+            subset.pop()
+            self.solve(ind+1,subset,nums,result) 
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        total_subset = 1 << n  # 2^n subsets
-        result = []           
-        
-        for num in range(total_subset):
-            lst = []          
-            for i in range(0, n):
-                if num & (1 << i) != 0:
-                    lst.append(nums[i])  # Include if bit is set
-            result.append(lst) 
-        
-        return result
+        result=[]
+        self.solve(0,[],nums,result)
+        return result  
